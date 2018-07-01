ae99445502418575f07c713258d96993d01c98dd
Nikita <nikitafefelov@gmail.com>
2018-07-01 10:54:45 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,116 @@
+var data = (localStorage.getItem('todoList')) ? JSON.parse(localStorage.getItem('todoList')) : {
+	todo: [],
+	completed: []
+};
+
+console.log(data);
+
+var removeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect class="noFill" width="22" height="22"/><g><g><path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6V18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/></g><g><path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>';
+var completeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect y="0" class="noFill" width="22" height="22"/><g><path class="fill" d="M9.7,14.4L9.7,14.4c-0.2,0-0.4-0.1-0.5-0.2l-2.7-2.7c-0.3-0.3-0.3-0.8,0-1.1s0.8-0.3,1.1,0l2.1,2.1l4.8-4.8c0.3-0.3,0.8-0.3,1.1,0s0.3,0.8,0,1.1l-5.3,5.3C10.1,14.3,9.9,14.4,9.7,14.4z"/></g></svg>';
+
+renderDOM();
+
+add.addEventListener('click', function() {
+	var value = document.getElementById('item').value;
+	if(value) addElement(value);
+});
+
+item.addEventListener('keydown', function(e) {
+	var value = this.value;
+	if(e.code === 'Enter' && value) 
+		addElement(value);
+});
+
+function addElement(value) {
+	document.getElementById('item').value = '';
+	addElementForDOM(value);
+	data.todo.push(value);
+	dataObjectUpdated();
+}
+
+function renderDOM() {
+	if(!data.todo.length && !data.completed.length) return;
+
+	for(let i = 0; i < data.todo.length; i++) {	
+		var value = data.todo[i];
+		addElementForDOM(value, 'todo');
+	}
+
+	for(let i = 0; i < data.completed.length; i++) {
+		var value = data.completed[i];
+		addElementForDOM(value, 'completed');
+	}
+}
+
+function dataObjectUpdated() {
+	localStorage.setItem('todoList', JSON.stringify(data));
+}
+
+function removeElement() {
+	var parent = this.parentNode.parentNode,
+		id = parent.parentNode.id,
+		value = parent.innerText,
+		arrayValue;
+
+	if(id === 'completed') {
+		data.completed.splice(data.completed.indexOf(value), 1);
+	} else if(id === 'todo') {
+		data.todo.splice(data.todo.indexOf(value), 1);
+	}
+	parent.remove();
+	dataObjectUpdated();
+}
+
+function completeElement() {
+	var parent = this.parentNode.parentNode,
+		id = parent.parentNode.id,
+		value = parent.innerText,
+		arrayValue;
+
+	if(id === 'completed') {
+		arrayValue = data.completed.indexOf(value);
+		todo.insertBefore(parent, todo.childNodes[0]);
+		data.completed.splice(arrayValue, 1);
+		data.todo.push(value);	
+	} else {
+		arrayValue = data.todo.indexOf(value);
+		completed.insertBefore(parent, completed.childNodes[0]);
+		data.todo.splice(arrayValue, 1);
+		data.completed.push(value);
+	}
+
+	dataObjectUpdated();
+}
+
+function addElementForDOM(value, list) {
+	var li = document.createElement('li');
+	li.innerText = value;
+
+	var div = document.createElement('div');
+	div.classList.add('buttons');
+	
+	var buttonRemove = document.createElement('button');
+	buttonRemove.classList.add('remove');
+	buttonRemove.innerHTML = removeSVG;
+
+	buttonRemove.addEventListener('click', removeElement);
+
+	var buttonComplete = document.createElement('button');
+	buttonComplete.classList.add('complete');
+	buttonComplete.innerHTML = completeSVG;
+
+	buttonComplete.addEventListener('click', completeElement);
+
+	li.appendChild(div);
+
+	div.appendChild(buttonRemove);
+	div.appendChild(buttonComplete);
+
+	todo.insertBefore(li, todo.childNodes[0]);
+
+	if(list) {
+		var todolist = document.getElementById(list);
+		todolist.appendChild(li);
+		todolist.insertBefore(li, todolist.childNodes[0]);
+	}
+}
